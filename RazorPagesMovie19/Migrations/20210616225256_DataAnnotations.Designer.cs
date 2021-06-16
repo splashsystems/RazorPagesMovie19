a37211804abee1c79073b3693903403faf6893a3
Scot <bscot.hagen@gmail.com>
2021-06-16 15:54:02 -0700

    Added Validations. Practice project complete

--- RazorPagesMovie19/Migrations/20210616225256_DataAnnotations.Designer.cs
+++ RazorPagesMovie19/Migrations/20210616225256_DataAnnotations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie19.Data;
 
 namespace RazorPagesMovie19.Migrations
 {
     [DbContext(typeof(RazorPagesMovie19Context))]
-    partial class RazorPagesMovie19ContextModelSnapshot : ModelSnapshot
+    [Migration("20210616225256_DataAnnotations")]
+    partial class DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
